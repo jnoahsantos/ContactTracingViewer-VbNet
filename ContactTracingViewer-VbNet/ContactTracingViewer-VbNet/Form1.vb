fd4e3690e1357f75b674b8da1eda5d8704953465
jnoahsantos <jamesnoah.santos27@gmail.com>
2022-06-20 18:34:55 +0800

    loades the json files from the folder of Contact Tracing Form

--- ContactTracingViewer-VbNet/ContactTracingViewer-VbNet/Form1.vb
+++ ContactTracingViewer-VbNet/ContactTracingViewer-VbNet/Form1.vb
@@ -1,3 +1,15 @@
-﻿Public Class ViewerApp
+﻿Imports System.IO
 
+Public Class ViewerApp
+
+    Shared filePathRecords As String = "C:\Users\Computer\Documents\ContactTracing-Logs\"
+    Shared files As String() = Directory.GetFiles(filePathRecords)
+
+    Private Sub app_loader(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each file As String In files
+            Dim filename As String = Path.GetFileName(file)
+            filename = filename.Replace(".txt", "")
+            listBox.Items.Add(filename)
+        Next
+    End Sub
 End Class
