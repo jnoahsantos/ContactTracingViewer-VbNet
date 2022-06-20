946b49cef8708c0f11eff568d4a631bec270b90b
jnoahsantos <jamesnoah.santos27@gmail.com>
2022-06-20 19:40:09 +0800

    Added NameSearch in folder

--- ContactTracingViewer-VbNet/ContactTracingViewer-VbNet/Form1.vb
+++ ContactTracingViewer-VbNet/ContactTracingViewer-VbNet/Form1.vb
@@ -12,4 +12,36 @@ Public Class ViewerApp
             listBox.Items.Add(filename)
         Next
     End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim keyWord As String = txtBxSearch.Text
+        Dim listOfNames As List(Of String) = New List(Of String)()
+        labelSearchWarning.Text = ""
+
+        For Each file As String In files
+            Dim filename As String = Path.GetFileName(file)
+            filename = filename.Replace(".txt", "")
+            listOfNames.Add(filename)
+        Next
+
+        Dim arrayOfNames As String() = listOfNames.ToArray()
+
+        If keyWord.Length >= 3 Then
+
+            For Each x As String In arrayOfNames
+
+                If (x.ToLower()).Contains(keyWord.ToLower()) Then
+                    listBox.Text = x
+                End If
+            Next
+
+            If Not ((listBox.Text).ToLower()).Contains(keyWord.ToLower()) Then
+                MessageBox.Show("Name Not Found", "Notification")
+            End If
+        Else
+            labelSearchWarning.Location = New Point(170, 106)
+            labelSearchWarning.Text = "Please enter atleast 3 character"
+        End If
+    End Sub
+
 End Class
